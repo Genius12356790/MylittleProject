f7046a4fa7512fde68e13dba5e024241a6d8ae54
Genius12356790 <78975138+Genius12356790@users.noreply.github.com>
2021-10-02 16:03:33 +0500

    v 0.4
    - Upgrade bug fixed
    - Added lives
    - Added custom non-playable objects

--- ScrollShooter/Scripts.py
+++ ScrollShooter/Scripts.py
@@ -2,3 +2,6 @@ def pow0(data):
     if data['level'] + 1 < len(data['levels']):
         data['level'] += 1
         data['bul'] = data['levels'][data['level']].copy()
+        
+def pow1up(data):
+    data['lives'] += 1
--- ScrollShooter/Shooter.py
+++ ScrollShooter/Shooter.py
@@ -6,13 +6,14 @@ import Scripts
 import random
 
 FRAMETIME = 1/60
-ENTITY_BASE_DATA = {'fire':0, 'fspd':0.5, 'vx':0, 'vy':0.5, 'dimg':0, 'bul':({'x': 0, 'y': 0, 'vx': 0, 'vy': 5, 'bimg': 1, 'dmg': 1}), 'simg':0, 'x':300, 'y':0, 'type':'enemy', 'hp':3, 'score':50, 'static':1}
+ENTITY_BASE_DATA = {'fire':0, 'fspd':0.5, 'vx':0, 'vy':0.5, 'dimg':0, 'bul':({'x': 0, 'y': 0, 'vx': 0, 'vy': 0.5, 'bimg': 1, 'dmg': 1}), 'simg':0, 'x':320, 'y':480, 'type':'enemy', 'hp':3, 'score':50, 'static':1, 'lives':0, 'level':0, 'levels':[], 'shield': 120}
+OBJECT_BASE_DATA = {'x': 0, 'y': 0, 'oimg': 0, 'kill': 0, 'show': 1}
 
 
 class Upgrade(pygame.sprite.Sprite):
     def __init__(self, x, y, name):
         super().__init__(upg)
-        self.data = entitys[name]
+        self.data = {a: entitys[name][a] for a in entitys[name]}  
         self.image = imagess[self.data['simg']]
         self.rect = self.image.get_rect().move(x, y) 
         self.data['x'] = x
@@ -24,18 +25,41 @@ class Upgrade(pygame.sprite.Sprite):
             self.rect.x = self.data['x'] - camx
             self.data['y'] += self.data['vy']
             self.rect.y = self.data['y']
+            if self.rect.y < -100 or self.rect.y > 640 or self.rect.x > 480 or self.rect.x < -100:
+                self.kill()
         if mode == 1:
             eval(self.data['func'])
             self.kill()
     
 
-
 class Object(pygame.sprite.Sprite):
-    def __init__(self, _, x, y):
+    def __init__(self, **kwargs):
         super().__init__(obj)
-        self.image = oimages[0]
-        self.image = self.image
-        self.rect = self.image.get_rect().move(x, y)
+        self.data = {}
+        self.setup(OBJECT_BASE_DATA)
+        self.setup(kwargs)
+        self.rect.x = self.data['x']
+        self.rect.y = self.data['y']
+        
+    def setup(self, kwargs):
+        for _ in kwargs:
+            self.data[_] = kwargs[_]
+            if _ == 'oimg':
+                self.image = oimages[self.data['oimg']]
+                self.data['size'] = self.image.get_rect().size
+                self.rect = self.image.get_rect()
+                
+    def update(self):
+        data = {a: self.data[a] for a in self.data}
+        data = eval(self.data['script'])
+        self.rect.x = data['x']
+        self.rect.y = data['y']
+        if data['show'] == 0:
+            self.image = deftex
+        else:
+            if data['oimg'] != self.data['oimg'] or self.data['show'] == 0:
+                self.image = oimages[data['oimg']]
+        self.data = {a: data[a] for a in data}
 
 
 def cut(image):
@@ -62,12 +86,11 @@ class Bullet(pygame.sprite.Sprite):
             self.data[_] = kwargs[_]
             if _ == 'bimg':
                 self.image = imagess[self.data['bimg']]
-                self.image = self.image
                 self.data['size'] = self.image.get_rect().size
                 self.rect = self.image.get_rect()
 
     def update(self, camx=0): # move&collidle
-        if self.rect.y < -100 or self.rect.y > 740 or self.rect.x > 740 or self.rect.x < -100:
+        if self.rect.y < -100 or self.rect.y > 640 or self.rect.x > 480 or self.rect.x < -100:
             self.kill()
         self.data['x'] += self.data['vx']
         self.rect.x = self.data['x'] - camx
@@ -88,6 +111,7 @@ class Entity(pygame.sprite.Sprite):
         self.setup(kwargs)
         if self.data['type'] == 'player':
             super().__init__(player)
+            self.basedata = {a: self.data[a] for a in self.data}
         else:
             super().__init__(enemy)
         
@@ -118,6 +142,7 @@ class Entity(pygame.sprite.Sprite):
             self.rect.x = self.data['x'] - camx
             self.rect.y = self.data['y']
         elif mode == 1: # tick
+            self.data['shield'] -= 1
             self.data['fire'] += self.data['fspd']
             if self.data['fire'] > 1:
                 self.data['fire'] -= 1
@@ -128,6 +153,7 @@ class Entity(pygame.sprite.Sprite):
             if self.data['type'] == 'player':
                 h = pygame.sprite.spritecollide(self, ebull, False)
                 if h:
+                    h[0].kill()
                     player.update(mode=3)
                 h = pygame.sprite.spritecollide(self, upg, False)
                 for a in h:
@@ -136,9 +162,14 @@ class Entity(pygame.sprite.Sprite):
         elif mode == 2: # setup
             self.setup(kwargs)
         elif mode == 3: # death
-            if self.data['type'] == 'player':
-                self.image = imagess[self.data['dimg']]
-                self.data['fspd'] = 0                
+            if self.data['type'] == 'player' and self.data['shield'] < 0:
+                if self.data['lives'] > 0:
+                    self.basedata['lives'] = self.data['lives'] - 1
+                    Entity(self.basedata)
+                    self.kill()
+                else:
+                    self.data['fspd'] = 0
+                    self.image = deftex
             elif self.data['type'] == 'enemy':
                 self.data['hp'] -= dmg
                 if self.data['hp'] < 1:
@@ -152,6 +183,8 @@ class Entity(pygame.sprite.Sprite):
             self.data['y'] += self.data['vy']
             self.rect.x = self.data['x'] - camx
             self.rect.y = self.data['y']
+            if self.rect.y < -100 or self.rect.y > 640 or self.rect.x > 480 or self.rect.x < -100:
+                self.kill()
         elif mode == 5: # entity with tiles
             if self.data['static']:
                 self.rect.y += 1
@@ -193,6 +226,7 @@ def play(spd=0.1):
     lastsec = 0
     while True:
         camx = player.sprites()[0].data['x'] // 4
+        life = player.sprites()[0].data['lives']
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
@@ -214,6 +248,7 @@ def play(spd=0.1):
                     keys[2] = False
                 if event.key == pygame.K_RIGHT:
                     keys[3] = False
+        #do tick
         player.update(keys=keys, camx=camx)
         player.update(mode=1, camx=camx)
         tick += spd
@@ -228,19 +263,20 @@ def play(spd=0.1):
                     mappos += 1
                 else:
                     break
-        s = score
+        '''s = score
         for sp in range(10):
             if s or not sp:
                 sc[sp].image = oimages[s % 10]
             else:
                 sc[sp].image = oimages[10]
-            s = s // 10
+            s = s // 10'''
         win.update(mode=1, camx=camx)
         bull.update(camx=camx)
         enemy.update(mode=1, camx=camx)
         enemy.update(camx=camx, mode=4)
         upg.update(camx=camx)
         obj.update()
+        #frame change
         while time.process_time() < lastframe + FRAMETIME:
             pass
         lastframe += FRAMETIME
@@ -279,8 +315,10 @@ def initgame():
             Tile(_, __)
     for _ in range(10):
         x = (9 - _) * 16
-        Object(_, x, 0)
+        Object(num=_, x=x, script="scoreobj(data, score)")
     sc = obj.sprites()
+    for _ in range(5):
+        Object(num=_, x=_ * 12, y=23, oimg=11, script="livesobj(data, player.sprites()[0].data['lives'])")
 
 
 def load_bg(spriteset):
@@ -318,6 +356,25 @@ def textout(text, pos, size=30, font='Comic Sans MS', color=(255, 255, 255)):
     myfont = pygame.font.SysFont(font, size)
     textsurface = myfont.render(text, False, color)
     screen.blit(textsurface,(pos))
+    
+def scoreobj(data, score):
+    s = '0'
+    if score > 0:
+        s = str(score)[::-1]
+    if len(s) > data['num']:
+        data['show'] = 1
+        data['oimg'] = int(s[data['num']])
+    else:
+        data['show'] = 0
+    return data
+
+def livesobj(data, lives):
+    if data['num'] < lives:
+        data['show'] = 1
+    else:
+        data['show'] = 0
+    return data
+    
 
 # init
 pygame.init()
@@ -331,7 +388,7 @@ bull = pygame.sprite.Group()
 ebull = pygame.sprite.Group()
 obj = pygame.sprite.Group()
 upg = pygame.sprite.Group()
-deftex = pygame.Surface((1, 1))
+deftex = pygame.Surface((0, 0))
 camx = 0
 score = 0
 bgx = 0
